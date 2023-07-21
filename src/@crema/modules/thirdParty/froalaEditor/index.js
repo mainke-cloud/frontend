3e5f64e967e5b7566c3f2f1922c97322952d830e
Crema Team <info@cremawork.com>
2023-07-21 13:50:52 +0530

    init CRA version 4.x

---
+++ src/@crema/modules/thirdParty/froalaEditor/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppComponentHeader from '@crema/components/AppComponentHeader';
+import AppGridContainer from '@crema/components/AppGridContainer';
+import Grid from '@mui/material/Grid';
+import AppComponentCard from '@crema/components/AppComponentCard';
+import SampleEditor from './Simple';
+
+const FroalaEditor = () => {
+  return (
+    <>
+      <AppComponentHeader
+        title='Froala WYSIWYG Editor'
+        description='React JS Froala WYSIWYG Editor'
+        refUrl='https://froala.com/wysiwyg-editor/docs/framework-plugins/react/'
+      />
+
+      <AppGridContainer>
+        <Grid item xs={12}>
+          <AppComponentCard
+            title='Classic'
+            description='This is your classic go-to editor - good for comment sections, forum post text areas, etc'
+            component={SampleEditor}
+          />
+        </Grid>
+      </AppGridContainer>
+    </>
+  );
+};
+
+export default FroalaEditor;
