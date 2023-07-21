2eda012b29a39267c1b50429c87cc6147d601d75
Crema Team <info@cremawork.com>
2023-07-21 14:38:58 +0530

    init CRA version 4.x

--- src/@crema/modules/apps/Calendar/TasksList/TaskCalendar/index.js
+++ src/@crema/modules/apps/Calendar/TasksList/TaskCalendar/index.js
@@ -16,76 +16,76 @@ const DragAndDropCalendar = withDragAndDrop(StyledCalendar);
 const localizer = dayjsLocalizer(dayjs);
 
 const TaskCalender = ({taskList, onUpdateTask, onSetFilterText}) => {
-    const navigate = useNavigate();
-    const {folder, label} = useParams();
-    const [selectedDate, setSelectedDate] = useState(null);
+  const navigate = useNavigate();
+  const {folder, label} = useParams();
+  const [selectedDate, setSelectedDate] = useState(null);
 
-    const onSelectDate = ({start}) => {
-        setSelectedDate(start);
-        // setAddTaskOpen(true);
-    };
+  const onSelectDate = ({start}) => {
+    setSelectedDate(start);
+    // setAddTaskOpen(true);
+  };
 
-    const onOpenAddTask = (data) => {
-        if (data) {
-            onViewTaskDetail(data);
-        } else {
-            if (selectedDate) {
-                console.log('selectedDate', selectedDate);
-            }
-        }
-    };
-    const resizeEvent = ({event, start, end}) => {
-        onUpdateTask({...event, startDate: start, endDate: end});
-    };
+  const onOpenAddTask = (data) => {
+    if (data) {
+      onViewTaskDetail(data);
+    } else {
+      if (selectedDate) {
+        console.log('selectedDate', selectedDate);
+      }
+    }
+  };
+  const resizeEvent = ({event, start, end}) => {
+    onUpdateTask({...event, startDate: start, endDate: end});
+  };
 
-    const onViewTaskDetail = (task) => {
-        if (folder) navigate(`/apps/calender/${folder}/${task.id}`);
-        if (label) navigate(`/apps/calender/label/${label}/${task.id}`);
-    };
-    const moveEvent = ({event, start, end}) => {
-        onUpdateTask({...event, startDate: start, endDate: end});
-    };
+  const onViewTaskDetail = (task) => {
+    if (folder) navigate(`/apps/calender/${folder}/${task.id}`);
+    if (label) navigate(`/apps/calender/label/${label}/${task.id}`);
+  };
+  const moveEvent = ({event, start, end}) => {
+    onUpdateTask({...event, startDate: start, endDate: end});
+  };
 
-    const getEvents = () => {
-        if (taskList?.length > 0)
-            return taskList.map((task) => {
-                return {
-                    ...task,
-                    title: task.title,
-                    start: task.startDate,
-                    end: task.endDate,
-                };
-            });
-        return [];
-    };
-    return (
-        <DragAndDropCalendar
-            localizer={localizer}
-            events={getEvents()}
-            themeVariant='dark'
-            views={['month', 'agenda']}
-            tooltipAccessor={null}
-            showMultiDayTimes
-            resizable
-            onEventResize={resizeEvent}
-            onEventDrop={moveEvent}
-            onSelectEvent={onOpenAddTask}
-            components={{
-                toolbar: (props) => (
-                    <CustomToolbar onSetFilterText={onSetFilterText} {...props} />
-                ),
-                event: (item) => <TaskItem key={item.key} item={item.event}/>,
-            }}
-            popup
-            selectable
-            onSelectSlot={onSelectDate}
-            defaultView='month'
-        />
-    );
+  const getEvents = () => {
+    if (taskList?.length > 0)
+      return taskList.map((task) => {
+        return {
+          ...task,
+          title: task.title,
+          start: task.startDate,
+          end: task.endDate,
+        };
+      });
+    return [];
+  };
+  return (
+    <DragAndDropCalendar
+      localizer={localizer}
+      events={getEvents()}
+      themeVariant='dark'
+      views={['month', 'agenda']}
+      tooltipAccessor={null}
+      showMultiDayTimes
+      resizable
+      onEventResize={resizeEvent}
+      onEventDrop={moveEvent}
+      onSelectEvent={onOpenAddTask}
+      components={{
+        toolbar: (props) => (
+          <CustomToolbar onSetFilterText={onSetFilterText} {...props} />
+        ),
+        event: (item) => <TaskItem key={item.key} item={item.event} />,
+      }}
+      popup
+      selectable
+      onSelectSlot={onSelectDate}
+      defaultView='month'
+    />
+  );
 };
 export default TaskCalender;
 TaskCalender.propTypes = {
-    taskList: PropTypes.any,
-    onUpdateTask: PropTypes.func,
-    onSetFilterText: PropTypes.func,
+  taskList: PropTypes.any,
+  onUpdateTask: PropTypes.func,
+  onSetFilterText: PropTypes.func,
 };
--- src/@crema/modules/thirdParty/fusionCharts/index.js
+++ src/@crema/modules/thirdParty/fusionCharts/index.js
@@ -18,75 +18,33 @@ import Grid from '@mui/material/Grid';
 
 const FusionCharts = () => {
   return (
-      <>
-        <AppComponentHeader
-            title='FusionCharts'
-            description='Build beautiful web & mobile dashboards'
-            refUrl='https://www.fusioncharts.com/react-charts?framework=react/'
-        />
+    <>
+      <AppComponentHeader
+        title='FusionCharts'
+        description='Build beautiful web & mobile dashboards'
+        refUrl='https://www.fusioncharts.com/react-charts?framework=react/'
+      />
 
-        <AppGridContainer>
-          <Grid item xs={12}>
-            <strong>
-              Code Commented due to some issue with Froala Editor at compile time.
-            </strong>
-            <pre>
+      <AppGridContainer>
+        <Grid item xs={12}>
+          <strong>
+            Code Commented due to some issue with Froala Editor at compile time.
+          </strong>
+          <pre>
             eslint-disable-next-line react/no-unescaped-entities Module not
             found: Error: Can&apos;t resolve &apos;crypto&apos; in
             eslint-disable-next-line react/no-unescaped-entities
-              &apos;...\node_modules\froala-editor\js&apos; BREAKING CHANGE:
+            &apos;...\node_modules\froala-editor\js&apos; BREAKING CHANGE:
             webpack &#60; 5 used to include polyfills for node.js core modules
             by default
           </pre>
 
-            <strong>
-              If you want to use this just uncomment the code and go ahead.
-            </strong>
-          </Grid>
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="Stacked Columns"*/}
-          {/*    component={StackedColumns}*/}
-          {/*    source={StackedColumnsSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="Stacked Negative"*/}
-          {/*    component={StackedNegative}*/}
-          {/*    source={StackedNegativeSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="Stacked Area"*/}
-          {/*    component={StackedArea}*/}
-          {/*    source={StackedAreaSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="Simple Chart"*/}
-          {/*    component={SimpleChart}*/}
-          {/*    source={SimpleChartSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="Simple Guage"*/}
-          {/*    component={SimpleGuage}*/}
-          {/*    source={SimpleGuageSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-          {/*<Grid item xs={12} lg={6}>*/}
-          {/*  <AppComponentCard*/}
-          {/*    title="WorldMap"*/}
-          {/*    component={WorldMap}*/}
-          {/*    source={WorldMapSource}*/}
-          {/*  />*/}
-          {/*</Grid>*/}
-        </AppGridContainer>
-      </>
+          <strong>
+            If you want to use this just uncomment the code and go ahead.
+          </strong>
+        </Grid>
+      </AppGridContainer>
+    </>
   );
 };
 
