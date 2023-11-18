1616189fd33b665109a6181632089afa9a80b3e2
Crema Team <info@cremawork.com>
2023-11-18 16:27:31 +0530

    remove mock api call from the sample page

--- src/modules/sample/Page1/index.js
+++ src/modules/sample/Page1/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { useGetDataApi } from '../../../@crema/hooks/APIHooks';
 
 const Page1 = () => {
-  const [{ apiData, loading }] = useGetDataApi('/api/sample', {}, {}, true);
-
-  console.log('apiData: ', apiData, loading);
   return (
     <>
       <h2>Sample page 1</h2>
