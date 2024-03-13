3c9083cb1fb9c5620b3fc28becbc5725e90456f4
henshammi <henshammi@gmail.com>
2024-03-13 23:21:59 +0700

    deleting files

--- src/modules/auth/Signin/KodeLogin.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const KodeLogin = () => {
-  return <div>ini kode login</div>;
-};
-
-export default KodeLogin;
--- src/modules/auth/Signin/NotifBerhasil.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotifBerhasil = () => {
-  return <div>ini notif berhasil</div>;
-};
-
-export default NotifBerhasil;
