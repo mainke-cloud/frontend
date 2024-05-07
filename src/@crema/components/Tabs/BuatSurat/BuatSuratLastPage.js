af39142fdd7d3f33db0ed3e20f278ae9f82eaf54
henshammi <henshammi@gmail.com>
2024-05-07 18:12:40 +0700

    update last page

--- src/@crema/components/Tabs/BuatSurat/BuatSuratLastPage.js
+++ src/@crema/components/Tabs/BuatSurat/BuatSuratLastPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PreviewSuratImage from '../../../../assets/BuatSurat/Preview Surat.png';
+import BuatSuratAlert from '@crema/components/Tabs/BuatSurat/BuatSuratAlert/BuatSuratAlert';
 
 const BuatSuratLastPage = () => {
   return (
     <div>
+      <BuatSuratAlert />
       <img
         src={PreviewSuratImage}
         alt='surat'
         style={{ paddingTop: '20px', maxWidth: '1305px' }}
-      />
+      ></img>
     </div>
   );
 };
