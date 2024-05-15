2567b3dd0ec83a550f9795aeaf4d9723cb69ce83
Nadhief Athallah Isya <nadhiefathallahi@gmail.com>
2024-05-15 14:39:28 +0700

    update api login using api online

--- src/modules/auth/Signin/SigninJwtAuth.js
+++ src/modules/auth/Signin/SigninJwtAuth.js
@@ -94,7 +94,7 @@ const SigninJwtAuth = () => {
 
       try {
         const response = await fetch(
-          'http://127.0.0.1:8000/api/auth/login/',
+          'https://new.coofis.com/api/auth/login/',
           {
             method: 'POST',
             headers: {
