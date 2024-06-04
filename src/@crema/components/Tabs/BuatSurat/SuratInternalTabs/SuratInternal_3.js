e823df3a3a8cb25fdd6ead31ea15a11ad7210f57
Nadhief Athallah Isya <nadhiefathallahi@gmail.com>
2024-06-04 14:16:38 +0700

    fix bug

--- src/@crema/components/Tabs/BuatSurat/SuratInternalTabs/SuratInternal_3.js
+++ src/@crema/components/Tabs/BuatSurat/SuratInternalTabs/SuratInternal_3.js
@@ -1,5 +1,5 @@
-import { IconButton, Link, Stack, TextField, Typography } from '@mui/material';
-import React from 'react';
+import { IconButton, Link, Stack, TextField, Typography , Box} from '@mui/material';
+import React  from 'react';
 import { Button } from '@mui/material';
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 import PropTypes from 'prop-types';
@@ -8,7 +8,8 @@ import { users } from '../../../../services/dummy/user/user';
 import { useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import AppScrollbar from '@crema/components/AppScrollbar';
 
 const SuratInternal_3 = ({ handleNext, handlePrev, onStateChange, templateData }) => {
   const [isComposeMail, setComposeMail] = React.useState(false);
@@ -196,6 +197,249 @@ const SuratInternal_3 = ({ handleNext, handlePrev, onStateChange, templateData }
       />
     </>
   );
+
+  // yg dikomen jangan di apus dulu soalnya gak tau temp datanya buat apa
+
+  // const formik = useFormik({
+  //   initialValues: {
+  //     jabatan: '',
+  //     nama: '',
+  //   },
+  //   onSubmit: handleNext,
+  //   validationSchema: yup.object().shape({
+  //     jabatan: yup.string().required('Kolom ini wajib diisi'),
+  //     nama: yup.string().required('Kolom ini wajib diisi'),
+  //   }),
+  // });
+  
+  // // const pengirim = useSelector((state) => state.addressbook.pengirim);
+  // const jabatann = useSelector((state) => state.addressbook.jabatann);
+  // let datass = jabatann[0];
+  // if (!datass || !Array.isArray(datass)) {
+  //   datass = [];
+  // }
+  // const namaa = useSelector((state) => state.addressbook.namaa);
+  // const initialState = useSelector((state) => state.surat);
+  // const [formData, setFormData] = useState(initialState);
+  // const [jabatanValue, setJabatanValue] = useState('');
+  // const [namaValue, setNamaValue] = useState('');
+
+  // useEffect(() => {
+  //   setJabatanValue(datass[0]?.jabatan ?? '');
+  //   setNamaValue(datass.map((item) => item.nama).join('\n'));
+  // }, [datass]);
+
+  // const handleJabatanChange = (event) => {
+  //   setJabatanValue(event.target.value);
+  // };
+
+  // const [isComposeMail, setComposeMail] = React.useState(false);
+  // const [composeMailTitle, setComposeMailTitle] = useState('');
+
+  // const onOpenComposeMail = (title) => {
+  //   setComposeMailTitle(title);
+  //   setJabatanValue('');
+  //   setNamaValue('');
+  //   setComposeMail(true);
+  // };
+
+  // const onCloseComposeMail = () => {
+  //   setJabatanValue(datass[0]?.jabatan ?? '');
+  //   setComposeMail(false);
+  // };
+
+  // return (
+  //   <>
+  //     <Stack
+  //       spacing={4}
+  //       sx={{
+  //         border: '1px solid #D8D8D8',
+  //         minHeight: '570px',
+  //         borderRadius: '12px',
+  //         padding: '15px',
+  //       }}
+  //     >
+  //       <Stack direction='row'>
+  //         <Link
+  //           component='button'
+  //           underline='always'
+  //           sx={{
+  //             fontSize: '16px',
+  //             fontWeight: 600,
+  //             color: 'black',
+  //             textDecorationColor: 'black',
+  //           }}
+  //           onClick={() => onOpenComposeMail('Jabatan')}
+  //         >
+  //           Jabatan
+  //         </Link>
+  //         <Typography variant='h4' color='red'>
+  //           *
+  //         </Typography>
+  //       </Stack>
+
+  //       <TextField
+  //         fullWidth
+  //         value={jabatanValue}
+  //         onChange={handleJabatanChange}
+  //         InputProps={{
+  //           endAdornment: (
+  //             <IconButton onClick={() => onOpenComposeMail('Jabatan')}>
+  //               <AddCircleOutlineRoundedIcon
+  //                 sx={{ color: 'black', fontSize: '40px' }}
+  //               />
+  //             </IconButton>
+  //           ),
+  //         }}
+  //         sx={{ color: 'black', fontWeight: 'bold' }}
+  //       />
+
+  //       <Stack direction='row'>
+  //         <Link
+  //           component='button'
+  //           underline='always'
+  //           sx={{
+  //             fontSize: '16px',
+  //             fontWeight: 600,
+  //             color: 'black',
+  //             textDecorationColor: 'black',
+  //           }}
+  //           onClick={() => onOpenComposeMail('Nama')}
+  //         >
+  //           Nama
+  //         </Link>
+  //         <Typography variant='h4' color='red'>
+  //           *
+  //         </Typography>
+  //       </Stack>
+
+  //       <Box position={'relative'}>
+  //         <AppScrollbar
+  //           sx={{
+  //             minHeight: '145px',
+  //             maxHeight: '145px',
+  //             overflow: 'auto',
+  //           }}
+  //         >
+  //           <Stack>
+  //             <TextField
+  //               id='outlined-multiline-static'
+  //               multiline
+  //               rows={5}
+  //               sx={{
+  //                 border: 'none',
+  //               }}
+  //               value={
+  //                 composeMailTitle === 'Nama' ? `${namaa.nama}` : namaValue
+  //               }
+  //               InputProps={{
+  //                 endAdornment: (
+  //                   <IconButton onClick={() => onOpenComposeMail('Nama')}>
+  //                     <AddCircleOutlineRoundedIcon
+  //                       sx={{ color: 'black', fontSize: '40px' }}
+  //                     />
+  //                   </IconButton>
+  //                 ),
+  //               }}
+  //             />
+  //           </Stack>
+  //         </AppScrollbar>
+  //       </Box>
+
+  //       <Typography variant='h4'>Divisi</Typography>
+
+  //       <TextField
+  //         fullWidth
+  //         value={
+  //           composeMailTitle === 'Nama'
+  //             ? `${namaa.divisi}`
+  //             : datass[0]?.divisi ?? ''
+  //         }
+  //       />
+
+  //       <Stack direction='row' spacing={5}>
+  //         <Stack flex={1} spacing={5}>
+  //           <Typography variant='h4'>NIK</Typography>
+  //           <TextField
+  //             value={
+  //               composeMailTitle === 'Nama'
+  //                 ? `${namaa.nikl}`
+  //                 : datass[0]?.nikl ?? ''
+  //             }
+  //           />
+  //         </Stack>
+  //         <Stack flex={1} spacing={5}>
+  //           <Typography variant='h4'>Kode Departemen</Typography>
+  //           <TextField
+  //             value={
+  //               composeMailTitle === 'Nama'
+  //                 ? `${namaa.kode_departemen}`
+  //                 : datass[0]?.kode_departemen ?? ''
+  //             }
+  //           />
+  //         </Stack>
+  //       </Stack>
+
+  //       <Typography variant='h4'>Departemen</Typography>
+
+  //       <TextField
+  //         fullWidth
+  //         value={
+  //           composeMailTitle === 'Nama'
+  //             ? `${namaa.departemen}`
+  //             : datass[0]?.departemen ?? ''
+  //         }
+  //       />
+
+  //       <Typography variant='h4'>Kota Kantor</Typography>
+
+  //       <TextField
+  //         fullWidth
+  //         value={
+  //           composeMailTitle === 'Nama'
+  //             ? `${namaa.kota}`
+  //             : datass[0]?.kota ?? ''
+  //         }
+  //       />
+  //     <Stack direction='row' justifyContent='flex-end' spacing={4}>
+  //          <Button
+  //           variant='contained'
+  //           sx={{
+  //             borderRadius: '12px',
+  //             bgcolor: '#D9DDE3',
+  //             color: '#5C5E61',
+  //             minWidth: '84px',
+  //           }}
+  //           onClick={handlePrev}
+  //         >
+  //           Kembali
+  //         </Button>
+
+  //         <Button
+  //           variant='contained'
+  //           sx={{
+  //             borderRadius: '12px',
+  //             bgcolor: '#E42313',
+  //             minWidth: '220px',
+  //             minHeight: '50px',
+  //           }}
+  //           onClick={formik.handleSubmit}
+  //         >
+  //           Selanjutnya (Penerima)
+  //         </Button>
+  //       </Stack>
+  //     </Stack>
+      
+
+  //     <ComposeMail
+  //       isComposeMail={isComposeMail}
+  //       onCloseComposeMail={onCloseComposeMail}
+  //       datas={users}
+  //       title={composeMailTitle}
+  //       type='single'
+  //     />
+  //   </>
+  // );
 };
 
 SuratInternal_3.propTypes = {
